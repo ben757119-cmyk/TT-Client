2a175c3be43725605a36efd476116659634e8288
ben757119-cmyk <ben757119@gmail.com>
2026-09-15 13:33:28 -0500

    Fix: do not register TTClient on event bus without @SubscribeEvent methods

--- src/main/java/com/ttclient/TTClient.java
+++ src/main/java/com/ttclient/TTClient.java
@@ -7,14 +7,13 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(TTClient.MOD_ID)
 public class TTClient {
     public static final String MOD_ID = "ttclient";
     public static final String NAME = "TT Client";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public static ModuleManager modules;
@@ -22,7 +21,6 @@ public class TTClient {
 
     public TTClient(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
-        NeoForge.EVENT_BUS.register(this);
         LOGGER.info("{} v{} initializing...", NAME, VERSION);
     }
 
